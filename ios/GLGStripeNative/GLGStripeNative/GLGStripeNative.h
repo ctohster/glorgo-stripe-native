d89689b643b297026e3f74ab2c725bc70c4f4b91
Vignesh Rajasekaran <rvignesh@outlook.com>
2018-05-15 11:35:01 +0800

    init glorgo stripe native

---
+++ ios/GLGStripeNative/GLGStripeNative/GLGStripeNative.h
@@ -0,0 +1,13 @@
+//
+//  GLGStripeNative.h
+//  GLGStripeNative
+//
+//  Created by Vignesh Rajasekaran on 15/5/18.
+//  Copyright © 2018 Glorgo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GLGStripeNative : NSObject
+
+@end
---
+++ ios/GLGStripeNative/GLGStripeNative/GLGStripeNative.m
@@ -0,0 +1,13 @@
+//
+//  GLGStripeNative.m
+//  GLGStripeNative
+//
+//  Created by Vignesh Rajasekaran on 15/5/18.
+//  Copyright © 2018 Glorgo. All rights reserved.
+//
+
+#import "GLGStripeNative.h"
+
+@implementation GLGStripeNative
+
+@end
